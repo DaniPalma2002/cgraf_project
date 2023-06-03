2b099e3ed0b1b1a2111a2f3494cd0046891ffe91
Daniel Pereira <danielppereira2002@tecnico.ulisboa.pt>
2023-06-03 17:04:27 +0100

    feat: add roof

--- Trabalho_C/js/main-script.js
+++ Trabalho_C/js/main-script.js
@@ -5,6 +5,8 @@ var camera;
 var scene, renderer;
 var controls;
 
+var house, roof;
+
 /////////////////////
 /* CREATE SCENE(S) */
 /////////////////////
@@ -12,7 +14,7 @@ function createScene(){
     'use strict';
     scene = new THREE.Scene();
     scene.background = new THREE.Color(0xF8F8F8);
-    scene.add(new THREE.AxesHelper(100));
+    scene.add(new THREE.AxesHelper(1000));
 }
 
 //////////////////////
@@ -21,7 +23,9 @@ function createScene(){
 function createCamera() {
     camera = new THREE.OrthographicCamera(-window.innerWidth / 2, window.innerWidth / 2,
         window.innerHeight / 2, -window.innerHeight / 2, 1, 1000);
-    camera.position.set(100, 100, 100);
+    camera.far = 10000;
+    camera.updateProjectionMatrix();
+    camera.position.set(200, 200, 200);
     camera.lookAt(scene.position);
 }
 
@@ -61,10 +65,10 @@ function createHouse() {
         330, 0, 0,  // Vertex 1
         330,  100, 0,  // Vertex 2
         0,  100, 0,  // Vertex 3
-        0, 0,  230,  // Vertex 4
-        330, 0,  230,  // Vertex 5
-        330,  100,  230,  // Vertex 6
-        0,  100,  230   // Vertex 7
+        0, 0,  -230,  // Vertex 4
+        330, 0,  -230,  // Vertex 5
+        330,  100,  -230,  // Vertex 6
+        0,  100,  -230,   // Vertex 7
     ];
     var positionAttribute = new THREE.Float32BufferAttribute(vertices, 3);
     geometry.setAttribute('position', positionAttribute);
@@ -79,8 +83,35 @@ function createHouse() {
     var indexAttribute = new THREE.Uint16BufferAttribute(indices, 1);
     geometry.setIndex(indexAttribute);
     var material = new THREE.MeshBasicMaterial({ color: 0xff0000, wireframe: true });
-    var cube = new THREE.Mesh(geometry, material);
-    scene.add(cube);
+    var house = new THREE.Mesh(geometry, material);
+    addRoof();
+    house.add(roof);
+    scene.add(house);
+}
+
+function addRoof() {
+    'use strict';
+    var geometry = new THREE.BufferGeometry();
+    var vertices = [
+        0, 100, 0,  // Vertex 0
+        330, 100, 0,  // Vertex 1
+        330,  160, -115,  // Vertex 2
+        0,  160, -115,  // Vertex 3
+        0, 100,  -230,  // Vertex 4
+        330, 100,  -230,  // Vertex 5
+    ];
+    var positionAttribute = new THREE.Float32BufferAttribute(vertices, 3);
+    geometry.setAttribute('position', positionAttribute);
+    var indices = [
+        1, 2, 0,  0, 2, 3,   // Face 0
+        2, 5, 3,  3, 5, 4,   // Face 1
+        0, 3, 4,             // Face 2
+        1, 5, 2,             // Face 3
+    ];
+    var indexAttribute = new THREE.Uint16BufferAttribute(indices, 1);
+    geometry.setIndex(indexAttribute);
+    var material = new THREE.MeshBasicMaterial({ color: 0x0000ff, wireframe: true });
+    roof = new THREE.Mesh(geometry, material);
 }
 
 //////////////////////
